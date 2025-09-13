66bb7957eae7e76f24db3b68b9a2fba57f34740d
LJAYi <37037437+LJAYi@users.noreply.github.com>
2025-09-14 02:03:02 +0800

    Update Region-Auto-Direct.js

--- Region-Auto-Direct.js
+++ Region-Auto-Direct.js
@@ -3,19 +3,19 @@
 // Author: LJAYi
 
 const MAP = {
-  CN:{group:"大陆场景", direct:"DIRECT",      proxy:"大陆时延优选"},
-  HK:{group:"香港场景", direct:"DIRECT",      proxy:"香港时延优选"},
-  TW:{group:"台湾场景", direct:"DIRECT",      proxy:"台湾时延优选"},
-  JP:{group:"日本场景", direct:"DIRECT",      proxy:"日本时延优选"},
-  KR:{group:"韩国场景", direct:"DIRECT",      proxy:"韩国时延优选"},
-  SG:{group:"新国场景", direct:"DIRECT",      proxy:"新国时延优选"},
-  US:{group:"美国场景", direct:"DIRECT",      proxy:"美国时延优选"},
+  CN:{group:"大陆场景", direct:"DIRECT", proxy:"大陆时延优选"},
+  HK:{group:"香港场景", direct:"DIRECT", proxy:"香港时延优选"},
+  TW:{group:"台湾场景", direct:"DIRECT", proxy:"台湾时延优选"},
+  JP:{group:"日本场景", direct:"DIRECT", proxy:"日本时延优选"},
+  KR:{group:"韩国场景", direct:"DIRECT", proxy:"韩国时延优选"},
+  SG:{group:"新国场景", direct:"DIRECT", proxy:"新国时延优选"},
+  US:{group:"美国场景", direct:"DIRECT", proxy:"美国时延优选"},
 };
 
 const URLS = [
   "https://ipapi.co/country",
   "https://ifconfig.co/country-iso",
-  "https://api.country.is/country"
+//  "https://api.country.is/country"
 ];
 
 const T=3000, NODE="DIRECT", KEY="RegionAutoDirect:last_cc";
